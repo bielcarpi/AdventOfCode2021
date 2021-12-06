41ceb6c33f9eafec68d32bd4dd411cabaea777eb
bielcarpi <biel.carpi@students.salle.url.edu>
2021-12-06 19:36:10 +0100

    Day 6 Part 2

--- src/Day6.java
+++ src/Day6.java
@@ -2,9 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Day6 {
     private static final int DAYS = 80;
+    private static final int DAYS_TWO = 256;
 
     public static void runOne(){
         ArrayList<Integer> values = getValues();
@@ -26,6 +28,40 @@ public class Day6 {
         }
 
         System.out.println("After " + DAYS + " days, there would be " + values.size() + " lanternfish");
+    }
+
+
+    public static void runTwo(){
+        ArrayList<Integer> initialValues = getValues();
+        if(initialValues == null) return;
+
+        //We'll store state in a hashmap
+        //The first int, the num of days that the lanternfish needs to create another lanternfish
+        //The second int, the num of lanternfish on that state
+        HashMap<Integer, Long> values = new HashMap<>();
+
+        //Fill the hashmap with initial values
+        for(int i = 0; i < 10; i++)
+            values.put(i, 0L);
+        for(int value: initialValues)
+            values.put(value, values.get(value) + 1);
+
+
+        for(int d = 0; d < DAYS_TWO; d++){
+            long dayZeroQuantity = values.get(0);
+            for(int i = 0; i < 9; i++)
+                values.put(i, values.get(i+1));
+
+            values.put(6, values.get(6) + dayZeroQuantity); //The ones with day zero, now day 5
+            values.put(8, dayZeroQuantity); //Add new lanternfish (each day-zero lanternfish creates a new one)
+        }
+
+
+        long totalLanternfish = 0;
+        for(int i: values.keySet())
+            totalLanternfish += values.get(i);
+
+        System.out.println("After " + DAYS_TWO + " days, there would be " + totalLanternfish + " lanternfish");
     }
 
     public static ArrayList<Integer> getValues(){
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Day6.runOne();
+        Day6.runTwo();
     }
 
 }
